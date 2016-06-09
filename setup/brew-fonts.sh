8291f950a07813ca299fc23d08b56d66ea84de8b
Jonathan Boiser <jboiser@gmail.com>
2016-06-09 00:05:15 -0500

    Brew fonts.

---
+++ setup/brew-fonts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+brew tap caskroom/fonts
+
+font-fantasque-sans-mono
+font-fira-code
+font-hack
+font-hasklig
+font-inconsolata-lgc
+font-mononoki
+font-roboto-mono
+font-source-code-pro
--- setup/brew-tools.sh
+++ setup/brew-tools.sh
@@ -5,7 +5,6 @@ brew install dark-mode
 brew install tree
 brew install ranger
 
-
 # emacs
 brew tap d12frosted/emacs-plus
 brew install emacs-plus --with-cocoa --with-gnutls --with-librsvg --with-imagemagick --with-spacemacs-icon
@@ -16,3 +15,5 @@ if [[ ! -d ~/.emacs.d ]]
 then
   git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
+
+brew cask install alfred
