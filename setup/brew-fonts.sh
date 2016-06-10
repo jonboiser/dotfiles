1f9b8666e554e80b8b93d9ea8e604df24df15122
Jonathan Boiser <jonathan.boiser@monsanto.com>
2016-06-10 18:03:15 -0500

    Fix brew fonts.

--- setup/brew-fonts.sh
+++ setup/brew-fonts.sh
@@ -2,12 +2,12 @@
 
 brew tap caskroom/fonts
 
-font-fantasque-sans-mono
-font-fira-code
-font-fira-mono
-font-hack
-font-hasklig
-font-inconsolata-lgc
-font-mononoki
-font-roboto-mono
-font-source-code-pro
+brew cask install font-fantasque-sans-mono
+brew cask install font-fira-code
+brew cask install font-fira-mono
+brew cask install font-hack
+brew cask install font-hasklig
+brew cask install font-inconsolata-lgc
+brew cask install font-mononoki
+brew cask install font-roboto-mono
+brew cask install font-source-code-pro
