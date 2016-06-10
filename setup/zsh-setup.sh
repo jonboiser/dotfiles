46e53d70fc40e309bd64fd464e317fbcaf227f2d
Jonathan Boiser <jonathan.boiser@monsanto.com>
2016-06-10 17:10:01 -0500

    Move zshrc

---
+++ setup/zsh-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# install zsh
+brew install zsh
+
+# change default shell
+sudo chsh -s /usr/local/bin/zsh
+
+# install antigen
+brew install antigen
+
+# install pure prompt
+nvm use stable
+npm i -g pure-prompt
+
+# other stuff
+brew install fzf
+/usr/local/opt/fzf/install
+
+brew install fasd
+
+# install rcm and symlink .zshrc
+brew install rcm
+rcup zshrc
+
+zsh ~/.zshrc
