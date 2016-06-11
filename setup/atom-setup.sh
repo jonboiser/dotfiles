e9a3bdb982fa3fda708cb05d9c01a81a3969000e
Jonathan Boiser <jonathan.boiser@monsanto.com>
2016-06-11 16:17:31 -0500

    Atom setup scripts.

---
+++ setup/atom-setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apm i advanced-open-file
+apm i atom-material-ui
+apm i autocomplete-paths
+apm i editorconfig
+apm i highlight-selected
+apm i last-cursor-position
+apm i linter
+apm i goto
+apm i pigments
+apm i project-manager
+apm i sort-lines
+apm i toggle-quotes
+apm i vim-mode
+apm i vim-surround
---
+++ setup/js-setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# atom packages
+apm i language-babel
+apm i hyperclick
+apm i js-hyperclick
+apm i linter-flow
+apm i linter-eslint
+apm i emmet
+
+# npm packages
+npm i -g eslint
+npm i -g flow-bin
