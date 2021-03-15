30d5cc05ca98fceefae468243b024e2a3ce74d3d
Jonathan Boiser <jboiser@gmail.com>
2021-03-14 23:01:49 -0700

    Add antibody files

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,7 +1,9 @@
-#!/bin/zsh
-
 function t() {
   # Defaults to 3 levels deep, do more with `t 5` or `t 1`
   # pass additional args after
   tree -I '.git|node_modules|bower_components|.DS_Store' --dirsfirst --filelimit 25 -L ${1:-2} -aC $2
+}
+
+function take() {
+  mkdir -p $@ && cd ${@:$#}
 }
